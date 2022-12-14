3ddb4cf8b2b38c6b0843abfac5f2067b403c59fd
AyanKhan665 <119148742+AyanKhan665@users.noreply.github.com>
2022-12-14 18:02:07 +0500

    Add files via upload

---
+++ run.py
@@ -0,0 +1,15 @@
+import os, platform
+try:
+    import requests
+except:
+    os.system('pip install requests')
+os.system('git pull')
+import requests
+os.system('git pull')
+bit = platform.architecture()[0]
+if bit == '64bit':
+    import o
+bit = platform.architecture()[0]
+if bit == '32bit':
+	print('Sorry Your Device Is Not Supported')
+	
